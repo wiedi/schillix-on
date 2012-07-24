76f4beadfae7563f68e76e077bf35a3966885c99
J. Schilling <joerg@schily.net>
2012-07-25 00:50:31 +0200

    662 PATH in /root/.profile should not have /usr/gnu/bin and should be compatible to the Bourne Shell Reviewed by: J. Schilling <joerg@schily.net> Approved by: J. Schilling <joerg@schily.net>

--- usr/src/cmd/nsadmin/dot-profile.sh
+++ usr/src/cmd/nsadmin/dot-profile.sh
@@ -1,16 +1,26 @@
 #
-# Simple profile places /usr/gnu/bin at front,
-# adds /usr/X11/bin, /usr/sbin and /sbin to the end.
-#
-# Use less(1) as the default pager for the man(1) command.
-#
-export PATH=/usr/gnu/bin:/usr/bin:/usr/sbin:/sbin
-export PAGER="/usr/bin/less -ins"
+# Simple profile. Do not place /usr/gnu/bin at front.
+# Add /usr/X11/bin, /usr/sbin and /sbin to the end.
+PATH=$PATH:/usr/sbin:/sbin
+export PATH
 
 #
+# Use less(1) as the default pager for the man(1) command.
+# ... but we do not have less on OpenSolaris-ON
+#
+#PAGER="/usr/bin/less -ins"
+PAGER="/usr/bin/more -s"
+export PAGER
+
+#
+# Only for ksh93...
 # Define default prompt to <username>@<hostname>:<path><"($|#) ">
 # and print '#' for user "root" and '$' for normal users.
 #
+if ( [ ! -z "${.sh.version}" ] ) 2>/dev/null ; then
+
 PS1='${LOGNAME}@$(/usr/bin/hostname):$(
     [[ "${LOGNAME}" == "root" ]] && printf "%s" "${PWD/${HOME}/~}# " ||
     printf "%s" "${PWD/${HOME}/~}\$ ")'
+
+fi
